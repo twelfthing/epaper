c6c8105288de3d20883b43c5cf42987067424a74
twelfthing <twelfthing@gmail.com>
2015-11-05 23:27:06 +0800

    init

---
+++ epaper/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for epaper project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'epaper'
+
+SPIDER_MODULES = ['epaper.spiders']
+NEWSPIDER_MODULE = 'epaper.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'epaper (+http://www.yourdomain.com)'
